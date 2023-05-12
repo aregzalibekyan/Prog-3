db3457e2cda896cec3d4fafd25e0999cfca37837
aregzalibekyan <areg.zalibekyan@tumo.org>
2023-05-12 15:48:06 +0400

    GameOfFinal

--- Predator.js
+++ Predator.js
@@ -1,9 +1,12 @@
-class Predator {
-    constructor(x, y) {
-        this.x = x
-        this.y = y
-        this.direction = []
+class Predator extends LivingCreature{
+    constructor(x, y,index) {
+        super(x,y,index)
         this.countEating = 0
+        this.direction = []
+        // this.x = x
+        // this.y = y
+        // this.direction = []
+        // this.countEating = 0
     }
 
     getNewCoordinates() {
@@ -20,18 +23,18 @@ class Predator {
     }
     chooseCell(character) {
         this.getNewCoordinates()
-       
-        var found = [];
-        for (var i in this.directions) {
-            var x = this.directions[i][0];
-            var y = this.directions[i][1];
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i]);
-                }
-            }
-        }
-        return found;
+       return super.chooseCell(character)
+        // var found = [];
+        // for (var i in this.directions) {
+        //     var x = this.directions[i][0];
+        //     var y = this.directions[i][1];
+        //     if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+        //         if (matrix[y][x] == character) {
+        //             found.push(this.directions[i]);
+        //         }
+        //     }
+        // }
+        // return found;
     }
 
     eat() {
